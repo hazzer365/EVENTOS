9f89cc34f31842d4bc309b16b6fe93f7229576ec
LUIS ANGEL TALAMANTE LARA <150637534+hazzer365@users.noreply.github.com>
2024-12-08 13:54:22 -0800

    Delete Eventos directory

--- Eventos/Javascript/eventos.js
+++
@@ -1,7 +0,0 @@
-// Asegúrate de que el script de Google Analytics gtag.js esté cargado antes de usarlo
-  document.getElementById('enviarButton').addEventListener('click', function() {
-    // Enviar el evento a Google Analytics
-    gtag('event', 'submit', {
-      'event_category': 'Formulario',
-      'event_action': 'Enviar',
-      'event_label': 'Botón ENVIAR'
